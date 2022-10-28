3f87e3e3028eb01096188e3ef7fd55aa73793085
FlorkGames <105853494+FlorkGames@users.noreply.github.com>
2022-10-28 12:22:15 -0300

    Delete assets/preload/data/pasta-night directory

--- assets/preload/data/pasta-night/note2.lua
+++
@@ -1,7 +0,0 @@
-function onCreate()
-    for i = 0, getProperty('unspawnNotes.length')-1 do
-        if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'plr3' then
-            setPropertyFromGroup('unspawnNotes', i, 'texture', 'pico');
-        end
-    end
-end
--- assets/preload/data/pasta-night/note3.lua
+++
@@ -1,7 +0,0 @@
-function onCreate()
-    for i = 0, getProperty('unspawnNotes.length')-1 do
-        if getPropertyFromGroup('unspawnNotes', i, 'noteType') == 'bf' then
-            setPropertyFromGroup('unspawnNotes', i, 'texture', 'bf');
-        end
-    end
-end
